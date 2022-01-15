22eb6d47b1f718d9b8d712fd2e3958d2b0d67c61
sawaTszm <sawa.tsuzumi@gmail.com>
2022-01-16 00:23:36 +0900

    ✨ add find_word.py

--- find_word/find_word.py
+++ find_word/find_word.py
@@ -1,5 +1,25 @@
+import csv
+import re
+from pprint import pprint
+
+
 def main():
-    pass
+    csv_file = open("./test_words.csv", "r")
+    f = csv.reader(csv_file, delimiter=",")
+    for row in f:
+        words = row
+
+    chara = input("探したい文字を入れてください: ")
+    search = "^(?=.*" + ")(?=.*".join(chara) + ").*$"
+    # ORの場合はre.search("f|t", word)の書き方ができるらしい
+    print(search)
+
+    match_words = []
+    for w in words:
+        if re.search(search, w):
+            match_words.append(w)
+
+    pprint(match_words, width=5)
 
 
 if __name__ == "__main__":
